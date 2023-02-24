fc469ec7abfb3b9c1cb6cc66cc63cd4f20687f5a
miaojingyi <miaojingyi86@gmail.com>
2023-02-24 14:35:38 +0800

    代码整理

--- src/library/diagram/index.js
+++ src/library/diagram/index.js
@@ -2,6 +2,5 @@ var diagram = {};
 
 diagram.diagram_designer = require("./designer");
 diagram.diagram_launch = require("./launch");
-diagram.diagram_full_designer = require("./full_designer");
 
 export { diagram };
