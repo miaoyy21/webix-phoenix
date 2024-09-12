45c269de66e4a378b22d8e79de92f5ed807eddea
MiaoJingyi <miaojingyi86@gmail.com>
2024-09-12 19:49:52 +0800

    .

--- src/dev/flow/jzwz/W01.js
+++ src/dev/flow/jzwz/W01.js
@@ -86,7 +86,7 @@ function builder(options, values) {
                     { view: "text", name: "gcmc", label: "项目名称", readonly: true },
                 ]
             },
-            { view: "textarea", name: "bz", label: "备注", placeholder: "请输入备注 ..." },
+            { view: "textarea", name: "bz", label: "备注", readonly: options["readonly"], placeholder: "请输入备注 ..." },
             {
                 cols: [
                     { view: "text", name: "cgy", label: "采购员", readonly: true },
@@ -113,36 +113,36 @@ function builder(options, values) {
             { id: "txmvalue", header: { text: "条形码", css: { "text-align": "center" } }, css: { "text-align": "center" }, width: 100 },
             { id: "jldw", header: { text: "单位", css: { "text-align": "center" } }, css: { "text-align": "center" }, width: 60 },
             {
-                id: "rksl", header: { text: "入库数量", css: { "text-align": "center" } }, editor: "text",
+                id: "rksl", header: { text: "入库数量", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null,
                 format: (value) => utils.formats.number.format(value, 2),
                 editParse: (value) => utils.formats.number.editParse(value, 2),
                 editFormat: (value) => utils.formats.number.editFormat(value, 2),
-                css: { "text-align": "right", "background": "#d5f5e3" },
+                css: { "text-align": "right", "background": !options["readonly"] ? "#d5f5e3" : null },
                 adjust: true, minWidth: 80
             },
             {
-                id: "cgdjhs", header: { text: "含税单价", css: { "text-align": "center" } }, editor: "text",
+                id: "cgdjhs", header: { text: "含税单价", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null,
                 format: (value) => utils.formats.number.format(value, 2),
                 editParse: (value) => utils.formats.number.editParse(value, 2),
                 editFormat: (value) => utils.formats.number.editFormat(value, 2),
-                css: { "text-align": "right", "background": "#d5f5e3" },
+                css: { "text-align": "right", "background": !options["readonly"] ? "#d5f5e3" : null },
                 adjust: true, minWidth: 80
             },
             {
-                id: "cgjehs", header: { text: "含税金额", css: { "text-align": "center" } }, editor: "text",
+                id: "cgjehs", header: { text: "含税金额", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null,
                 format: (value) => utils.formats.number.format(value, 2),
                 editParse: (value) => utils.formats.number.editParse(value, 2),
                 editFormat: (value) => utils.formats.number.editFormat(value, 2),
                 footer: { content: "summColumn", css: { "text-align": "right" } },
-                css: { "text-align": "right", "background": "#d5f5e3" },
+                css: { "text-align": "right", "background": !options["readonly"] ? "#d5f5e3" : null },
                 adjust: true, minWidth: 80
             },
             {
-                id: "taxrate", header: { text: "税率(%)", css: { "text-align": "center" } }, editor: "text",
+                id: "taxrate", header: { text: "税率(%)", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null,
                 format: (value) => utils.formats.number.format(value, 2),
                 editParse: (value) => utils.formats.number.editParse(value, 2),
                 editFormat: (value) => utils.formats.number.editFormat(value, 2),
-                css: { "text-align": "right", "background": "#d5f5e3" },
+                css: { "text-align": "right", "background": !options["readonly"] ? "#d5f5e3" : null },
                 adjust: true, minWidth: 60
             },
             {
@@ -164,10 +164,10 @@ function builder(options, values) {
                 css: { "text-align": "right" },
                 adjust: true, minWidth: 80
             },
-            { id: "sccjmc", header: { text: "生产厂家", css: { "text-align": "center" } }, editor: "text", width: 160 },
-            { id: "clph", header: { text: "材料批号", css: { "text-align": "center" } }, editor: "text", width: 120 },
+            { id: "sccjmc", header: { text: "生产厂家", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null, width: 160 },
+            { id: "clph", header: { text: "材料批号", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null, width: 120 },
             {
-                id: "scrq", header: { text: "生产日期", css: { "text-align": "center" } }, editor: "date",
+                id: "scrq", header: { text: "生产日期", css: { "text-align": "center" } }, editor: !options["readonly"] ? "date" : null,
                 format: utils.formats.date.format,
                 editParse: utils.formats.date.editParse,
                 editFormat: utils.formats.date.editFormat,
@@ -177,7 +177,7 @@ function builder(options, values) {
             { id: "byyq", header: { text: "检验要求", css: { "text-align": "center" } }, minWidth: 240, maxWidth: 360 },
             { id: "ckmc", header: { text: "默认仓库", css: { "text-align": "center" } }, width: 120 },
             { id: "kwmc", header: { text: "默认库位", css: { "text-align": "center" } }, width: 160 },
-            { id: "bz", header: { text: "备注", css: { "text-align": "center" } }, editor: "text", width: 240 },
+            { id: "bz", header: { text: "备注", css: { "text-align": "center" } }, editor: !options["readonly"] ? "text" : null, width: 240 },
             {
                 id: "buttons",
                 width: 80,
@@ -204,6 +204,13 @@ function builder(options, values) {
                         $$(mxGrid.id).refresh(id);
                     })
             },
+            onAfterRender() {
+                if (options["readonly"]) {
+                    mxGrid.actions.hideColumn("buttons", true);
+                } else {
+                    mxGrid.actions.hideColumn("buttons", false);
+                }
+            }
         }
     });
 
@@ -347,6 +354,38 @@ function builder(options, values) {
         }).show();
     }
 
+
+    var btnWzdm = {
+        view: "button", label: "选择物资", autowidth: true, css: "webix_primary", type: "icon", icon: "mdi mdi-18px mdi-gesture-tap-hold",
+        click() {
+            var values = $$(mxGrid.id).serialize(true);
+
+            // 选择物资代码
+            utils.windows.wzdm({
+                multiple: true,
+                checked: [],
+                filter: (row) => (row["xyzt"] != '禁用' && _.findIndex(values, (value) => (value["wzbh"] == row["wzbh"])) < 0),
+                callback(checked) {
+                    // 获取条形码
+                    var request = webix.ajax().sync().get("api/sys/auto_nos", { "method": "Patch", "code": "txmvalue", "count": _.size(checked) });
+                    var txmvalues = JSON.parse(request.responseText);
+
+                    _.each(checked, (row, index) => {
+                        var newRow = _.pick(row, "wzbh", "wzmc", "ggxh", "wzph", "bzdh", "jldw", "sccjmc", "bylx", "byyq", "ckbh", "ckmc", "kwbh", "kwmc");
+                        newRow = _.extend(newRow, { "txmvalue": txmvalues[index], "rksl": 0, "cgdjhs": 0, "cgjehs": 0, "taxrate": 13, "cgdj": 0, "cgje": 0, "taxje": 0 });
+
+                        $$(mxGrid.id).add(newRow);
+                    });
+                    webix.message({ type: "success", text: "选择" + _.size(checked) + "条物资！" });
+
+                    return true;
+                }
+            })
+        }
+    };
+
+    var btnImport = utils.protos.importExcelButton({ label: "物资导入", onImport: openImport });
+
     // 请假单
     return {
         show() {
@@ -364,39 +403,12 @@ function builder(options, values) {
                                     gravity: 2,
                                     rows: [
                                         {
-                                            view: "toolbar", cols: [
-                                                {
-                                                    id: btnMxWzdm, view: "button", label: "选择物资", autowidth: true, css: "webix_primary", type: "icon", icon: "mdi mdi-18px mdi-gesture-tap-hold",
-                                                    click() {
-                                                        var values = $$(mxGrid.id).serialize(true);
-
-                                                        // 选择物资代码
-                                                        utils.windows.wzdm({
-                                                            multiple: true,
-                                                            checked: [],
-                                                            filter: (row) => (row["xyzt"] != '禁用' && _.findIndex(values, (value) => (value["wzbh"] == row["wzbh"])) < 0),
-                                                            callback(checked) {
-                                                                // 获取条形码
-                                                                var request = webix.ajax().sync().get("api/sys/auto_nos", { "method": "Patch", "code": "txmvalue", "count": _.size(checked) });
-                                                                var txmvalues = JSON.parse(request.responseText);
-
-                                                                _.each(checked, (row, index) => {
-                                                                    var newRow = _.pick(row, "wzbh", "wzmc", "ggxh", "wzph", "bzdh", "jldw", "sccjmc", "bylx", "byyq", "ckbh", "ckmc", "kwbh", "kwmc");
-                                                                    newRow = _.extend(newRow, { "txmvalue": txmvalues[index], "rksl": 0, "cgdjhs": 0, "cgjehs": 0, "taxrate": 13, "cgdj": 0, "cgje": 0, "taxje": 0 });
-
-                                                                    $$(mxGrid.id).add(newRow);
-                                                                });
-                                                                webix.message({ type: "success", text: "选择" + _.size(checked) + "条物资！" });
-
-                                                                return true;
-                                                            }
-                                                        })
-                                                    }
-                                                },
+                                            view: "toolbar", cols: !options["readonly"] ? [
+                                                btnWzdm,
                                                 { width: 24 },
-                                                utils.protos.importExcelButton({ id: btnMxImport, label: "物资导入", onImport: openImport }),
+                                                btnImport,
                                                 {}
-                                            ]
+                                            ] : [{ view: "label", label: "<span style='margin-left:8px'></span>物资入库单明细", height: 38 }]
                                         },
                                         mxGrid,
                                     ]
