1ae0b2265ba863fd8b65c88b8cdad784b8c16793
MiaoJingyi <miaojingyi86@gmail.com>
2024-09-25 22:03:40 +0800

    .

--- src/utils/protos/signer.js
+++ src/utils/protos/signer.js
@@ -3,7 +3,7 @@ function signer(user) {
     if (_.size(user) < 1) {
         return {
             view: "template",
-            template: "<div class='webix_light'> - - - </div>",
+            template: "<div class='webix_light'> - </div>",
             width: 120
         };
     }
@@ -11,7 +11,7 @@ function signer(user) {
     return {
         view: "template",
         borderless: true,
-        template: "<img src='/api/sys/docs?method=Signer&user=" + user + "' style='width:100%; height:100%; object-position:left; object-fit:contain'>",
+        template: `<img src='/api/sys/docs?method=Signer&user=` + user + `' style='width:100%; height:100%; object-position:left; object-fit:contain' onerror='this.src = "/assets/signer_none.png"'>`,
         width: 120,
     };
 };
