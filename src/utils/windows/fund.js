cd944fcfbfefdca71ece4a26d539c540018aed45
MiaoJingyi <miaojingyi86@gmail.com>
2025-09-16 21:36:02 +0800

    .

--- src/utils/windows/fund.js
+++ src/utils/windows/fund.js
@@ -2,7 +2,7 @@
 import { formats } from "../etc/formats"
 
 // 默认状态
-const DefaultOptions = { multiple: false, cache: true, checked: [], filter: (row) => true, callback: (select) => { } };
+const DefaultOptions = { filter: (row) => true, callback: (select) => { } };
 
 const instance = {
     window_id: "phoenix_utils_windows_fund",
@@ -11,34 +11,28 @@ const instance = {
     filter_placeholder: "请输入基金代码、基金名称",
 
     grid_id: "phoenix_utils_windows_fund_grid",
-
-    toolbar_checked_id: "phoenix_utils_windows_fund_toolbar_checked",
-    checked_id: "phoenix_utils_windows_fund_checked",
+    pager_id: "phoenix_utils_windows_fund_pager",
 
     // 状态设置
-    allData: [],
     options: DefaultOptions,
 }
 
 // 刷新
-instance.reload = function (force) {
-    if (force) {
-        var request = webix.ajax().sync().get("/api/sys/data_service?service=ST_FUND.query");
-        instance.allData = JSON.parse(request.responseText)["data"];
-    }
+instance.reload = function () {
+    var request = webix.ajax().sync().get("/api/sys/data_service?service=ST_FUND.query");
+    var allData = JSON.parse(request.responseText)["data"];
 
     // 根据条件进行数据筛选
-    var data = _.filter(instance.allData, (row) => instance.options.filter(row));
+    var data = _.filter(allData, (row) => instance.options.filter(row));
 
     // 设置选中状态
     $$(instance.grid_id).clearAll();
 
-    var rows = _.map(data, (row) => _.extend(row, { "checked": _.findIndex(instance.options.checked, (obj) => row["code"] == obj["code"]) >= 0 }));
-    $$(instance.grid_id).define("data", rows)
+    $$(instance.grid_id).define("data", data)
     $$(instance.grid_id).refresh();
 
     // 默认选中第1个可选用户
-    if ($$(instance.grid_id).select && $$(instance.grid_id).getFirstId()) {
+    if ($$(instance.grid_id).getFirstId()) {
         $$(instance.grid_id).select($$(instance.grid_id).getFirstId());
     }
 
@@ -46,14 +40,11 @@ instance.reload = function (force) {
     if ($$(instance.filter_id).getValue()) {
         instance.filter();
     }
-
-    // 设置已选用户
-    $$(instance.checked_id).setValue(_.pluck(instance.options.checked, "name").join(","));
 }
 
 // 过滤
 instance.filter = function () {
-    $$(instance.grid_id).filter((row) => ((row["code"] || "") + "|" + (row["name"] || "")).indexOf($$(instance.filter_id).getValue()) >= 0);
+    $$(instance.grid_id).filter((row) => ((row["code"] || "") + "|" + (row["name"] || "")).toUpperCase().indexOf($$(instance.filter_id).getValue().toUpperCase()) >= 0);
 
     // 没有符合条件的数据
     if (!$$(instance.grid_id).count()) {
@@ -62,18 +53,13 @@ instance.filter = function () {
         $$(instance.grid_id).hideOverlay();
 
         // 默认选中第1个可选用户
-        if ($$(instance.grid_id).select) {
-            $$(instance.grid_id).select($$(instance.grid_id).getFirstId());
-        }
+        $$(instance.grid_id).select($$(instance.grid_id).getFirstId());
     }
 }
 
 // 确定
 instance.ok = function () {
-    var checked = instance.options.checked;
-    if (!instance.options.multiple) {
-        checked = $$(instance.grid_id).getSelectedItem(false);
-    }
+    var checked = $$(instance.grid_id).getSelectedItem(false);
 
     // 回调成功，隐藏对话框
     if (instance.options.callback(checked)) {
@@ -89,8 +75,8 @@ webix.ui({
     modal: true,
     close: true,
     move: true,
-    height: 420,
-    width: 600,
+    height: 600,
+    width: 800,
     headHeight: 38,
     position: "center",
     head: "选择基金",
@@ -99,38 +85,25 @@ webix.ui({
         rows: [
             {
                 rows: [
-                    { id: instance.filter_id, view: "text", placeholder: instance.filter_placeholder, clear: true, on: { onChange: instance.filter } },
+                    { id: instance.filter_id, view: "text", keyPressTimeout: 250, placeholder: instance.filter_placeholder, clear: true, on: { onTimedKeyPress: instance.filter } },
                     {
                         id: instance.grid_id,
                         view: "datatable",
                         css: "webix_data_border webix_header_border",
-                        multiselect: true,
+                        select: "row",
                         headerRowHeight: 35,
                         rowHeight: 28,
-                        checkboxRefresh: true,
                         data: [],
                         columns: [
                             { id: "index", header: { text: "№", css: { "text-align": "center" } }, css: { "text-align": "center" }, width: 50 },
-                            {
-                                id: "checked", header: { text: "选择", css: { "text-align": "center" } }, css: { "text-align": "center" },
-                                template(obj, common, value) {
-                                    if (value) {
-                                        return "<span class='webix_table_checkbox checked webix_icon phoenix_primary_icon mdi mdi-checkbox-marked' />";
-                                    }
-
-                                    return "<span class='webix_table_checkbox notchecked webix_icon mdi mdi-checkbox-blank-outline'/>";
-                                }, width: 50
-                            },
-                            { id: "code", header: { text: "基金代码", css: { "text-align": "center" } }, sort: "text", css: { "text-align": "center" }, width: 120 },
+                            { id: "code", header: { text: "基金代码", css: { "text-align": "center" } }, sort: "text", css: { "text-align": "center" }, width: 100 },
                             { id: "name", header: { text: "基金名称", css: { "text-align": "center" } }, sort: "text", minWidth: 240, fillspace: true },
                             { id: "npv", header: { text: "净值", css: { "text-align": "center" } }, format: (value) => formats.number.format(value, 4), sort: "int", css: { "text-align": "right" }, minWidth: 80 },
                         ],
                         elementsConfig: { labelAlign: "right", clear: false },
                         on: {
                             "data->onStoreUpdated": function () {
-                                this.data.each(function (obj, i) {
-                                    obj.index = i + 1;
-                                })
+                                this.data.each((obj, i) => { obj.index = i + 1; })
                             },
                             onBeforeLoad() {
                                 this.showOverlay("数据加载中...");
@@ -142,31 +115,46 @@ webix.ui({
                                     return;
                                 }
                             },
-                            onCheck(id, col, value) {
-                                var row = this.getItem(id);
-                                if (!row) return;
-
-                                if (value) {
-                                    instance.options.checked.push(row);
-                                } else {
-                                    instance.options.checked =
-                                        _.reject(instance.options.checked, (user) => user["code"] == row["code"]);
-                                }
-
-                                $$(instance.checked_id).setValue(_.pluck(instance.options.checked, "name").join(","));
-                            }
-                        }
+                            onItemDblClick: instance.ok
+                        },
+                        pager: instance.pager_id,
                     },
+                    {
+                        id: instance.pager_id,
+                        view: "pager",
+                        borderless: false,
+                        css: { "border-top": "none", "border-right": "none", "border-bottom": "none" },
+                        template: function (obj, common) {
+                            var master = $$(obj.id).$master;
+
+                            switch (master.config.view) {
+                                case "datatable":
+                                    if (master.count()) {
+                                        return common.first(obj) + common.prev(obj) + common.pages(obj) + common.next(obj) + common.last(obj)
+                                            + "  当前 第" + common.page(obj) + "页，总共 " + obj.limit + "页，共计 " + webix.i18n.intFormat(obj.count) + "条记录。";
+                                    }
+
+                                    return "<span style='margin-left:24px'>共计 0条记录</span>";
+                                case "editlist":
+                                case "list":
+                                    if (master.count()) {
+                                        return common.first(obj) + common.prev(obj) + "<span style='margin-left:24px;margin-right:24px'>第" + common.page(obj) + "/" + obj.limit + "页</span>" + common.next(obj) + common.last(obj);
+                                    }
+
+                                    return "";
+                                default:
+                                    return common.first(obj) + common.prev(obj) + common.next(obj) + common.last(obj);
+                            }
+                        },
+                        group: 5,
+                        size: 50
+                    }
                 ]
-            },
-            {
-                id: instance.toolbar_checked_id,
-                cols: [{ id: instance.checked_id, view: "text", label: "已选基金", labelAlign: "right", readonly: true, placeholder: "请选择基金..." },]
             },
             {
                 cols: [
                     { width: 8 },
-                    { view: "button", label: "刷新", minWidth: 88, autowidth: true, css: "webix_transparent", click: () => instance.reload(true) },
+                    { view: "button", label: "刷新", minWidth: 88, autowidth: true, css: "webix_transparent", click: instance.reload },
                     {},
                     { view: "button", label: "确定", minWidth: 88, autowidth: true, css: "webix_primary", click: instance.ok },
                     { width: 8 }
@@ -179,10 +167,7 @@ webix.ui({
 
 /*
     {
-        multiple    可选    是否启用多选，默认单选
-        cache       可选    是否开启缓存数据，默认开启
         filter      可选    筛选数据回调函数
-        checked     可选    已选基金ID，[{"code":"基金代码","name":"基金名称"}, ...]
         callback    必须    点击确定的回调函数
     }
 */
@@ -191,51 +176,9 @@ export function fund(options) {
     // 参数配置
     instance.options = _.extend({}, DefaultOptions, options);
 
-    // 返回数组格式
-    if (_.isEmpty(instance.options.checked)) {
-        instance.options.checked = [];
-    } else if (!_.isArray(instance.options.checked)) {
-        console.error(`checked must be a Array, like [{"code":"基金代码","name":"基金名称"}, ...]`)
-        return
-    } else {
-        instance.options.checked = _.reject(instance.options.checked, (o) => _.isEmpty(o.id));
-    }
-
-    // 是否多选
-    if (instance.options.multiple) {
-        if (!$$(instance.grid_id).isColumnVisible("checked")) {
-            $$(instance.grid_id).showColumn("checked");
-        }
-
-        $$(instance.grid_id).define({ select: false });
-        $$(instance.toolbar_checked_id).show();
-    } else {
-        if ($$(instance.grid_id).isColumnVisible("checked")) {
-            $$(instance.grid_id).hideColumn("checked");
-        }
-
-        $$(instance.grid_id).define({ select: "row" });
-        $$(instance.toolbar_checked_id).hide();
-    }
-    $$(instance.grid_id).refresh();
-
     // 如果未设置缓存，或者无数据，那么执行刷新
-    if (!instance.options.cache || $$(instance.grid_id).count() < 1) {
-        instance.reload(true);
-    } else {
-        instance.reload(false);
-
-        // 重新设置用户选中状态
-        $$(instance.grid_id).eachRow(
-            function (id) {
-                var row = this.getItem(id);
-                row.checked = _.findIndex(instance.options.checked, (obj) => row["code"] == obj["code"]) >= 0;
-
-                this.updateItem(id, row);
-            }, true);
-
-        // 设置已选用户
-        $$(instance.checked_id).setValue(_.pluck(instance.options.checked, "name").join(","));
+    if ($$(instance.grid_id).count() < 1) {
+        instance.reload();
     }
 
     $$(instance.window_id).show();
